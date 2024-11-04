1a2198e089484743f3f8817928b430d5c883ef5d
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2024-11-04 16:41:17 +0200

    added tests and coverage check

---
+++ vko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,36 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+  def get_players(self):
+    return [
+      Player('Bärlund', 'A', 1, 2),
+      Player('Helganatt', 'A', 0, 29),
+      Player('Pestosås', 'B', 7, 0),
+      Player('Kartongsmakande', 'C', 0, 0)
+    ]
+  
+class TestStatisticsService(unittest.TestCase):
+  def setUp(self):
+    self.a = StatisticsService(PlayerReaderStub())
+
+  def test_a(self):
+    q = self.a.search('Bärlund')
+    self.assertEqual(q.name, 'Bärlund')
+
+  def test_b(self):
+    q = self.a.search('Mustakallio')
+    self.assertEqual(q, None)
+
+  def test_c(self):
+    q = self.a.team('A')
+    w = ['Bärlund', 'Helganatt']
+    e = [x.name for x in q]
+    self.assertEqual(e, w)
+
+  def test_d(self):
+    q = self.a.top(2)
+    w = ['Helganatt', 'Pestosås']
+    e = [x.name for x in q]
+    self.assertEqual(w, e)
