665a3be01d3809eee9d5a2502efd5d8d509ea7ac
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2024-11-11 19:29:29 +0200

    added files, made some changes

---
+++ vko2/nhl-reader/src/index.py
@@ -0,0 +1,29 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    # print("JSON-muotoinen vastaus:")
+    # print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    # print("Oliot:")
+
+    a = []
+
+    for x in players:
+        if x.a == 'FIN':
+            a.append(x)
+
+    for player in a:
+        print(player)
+
+if __name__ == "__main__":
+    main()
