701d08d2f3bd5286bdfa61f1804067847a087153
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2024-11-11 17:32:38 +0200

    use modules

--- vko2/git-branch-harjoitus/index.py
+++ vko2/git-branch-harjoitus/index.py
@@ -1,8 +1,13 @@
 from logger import logger
+from summa import summa
+from erotus import erotus
 
 logger('aloitetaan')
 
 x = int(input('luku 1: '))
 y = int(input('luku 2: '))
+
+print(summa(x, y))
+print(erotus(x, y))
 
 logger('lopetetaan')
