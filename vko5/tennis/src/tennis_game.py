fa66c1f737e995b2101bad9a873622680406ee4f
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2024-12-02 13:00:18 +0200

    small refactoring to make code more readable

--- vko5/tennis/src/tennis_game.py
+++ vko5/tennis/src/tennis_game.py
@@ -2,54 +2,51 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
 
-    def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
-        else:
-            self.m_score2 = self.m_score2 + 1
+    def won_point(self, which):
+        if which == self.player1_name:
+            self.player1_score += 1
+        else: # player 2
+            self.player2_score += 1
+
+    def _get_player_score_text(self, score):
+        if score == 0:
+            return 'Love'
+        elif score == 1:
+            return 'Fifteen'
+        elif score == 2:
+            return 'Thirty'
+        elif score == 3:
+            return 'Forty'
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        score = ''
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
+        if self.player1_score == self.player2_score:
+            if self.player1_score == 0: # both 0
+                score = 'Love-All'
+            elif self.player1_score == 1: # both 1
+                score = 'Fifteen-All'
+            elif self.player1_score == 2: # both 2
+                score = 'Thirty-All'
+            else: # both 4
+                score = 'Deuce'
+        elif self.player1_score >= 4 or self.player2_score >= 4:
+            player1_player2_difference = self.player1_score - self.player2_score
+
+            if player1_player2_difference == 1:
+                score = 'Advantage player1'
+            elif player1_player2_difference == -1:
+                score = 'Advantage player2'
+            elif player1_player2_difference >= 2:
+                score = 'Win for player1'
             else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+                score = 'Win for player2'
+        else: # scores under 4 and not same
+            score = f'{self._get_player_score_text(self.player1_score)}' \
+                    '-' \
+                    f'{self._get_player_score_text(self.player2_score)}'
 
         return score
