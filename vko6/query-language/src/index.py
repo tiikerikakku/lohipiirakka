a33d368b8eb9988f072b36c095c7be8eed7a6e8b
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2024-12-09 13:43:13 +0200

    some testing

--- vko6/query-language/src/index.py
+++ vko6/query-language/src/index.py
@@ -1,17 +1,23 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn
+from query_builder import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
-    )
+    # matcher = And(
+    #     HasAtLeast(5, "goals"),
+    #     HasAtLeast(20, "assists"),
+    #     PlaysIn("PHI")
+    # )
+
+    query = QueryBuilder()
+    matcher = query.plays_in("NYR").has_at_least(10, "goals").has_fewer_than(20, "goals").build()
+
+    # print(len(stats.matches(matcher)))
 
     for player in stats.matches(matcher):
         print(player)
